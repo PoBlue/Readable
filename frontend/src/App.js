fd8f9f7fa49f3308b072937cd6ac8d8ce36abeb7
PoBlue <250299430@qq.com>
2017-10-14 14:47:17 +0800

    defined all api request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as API from './api/Api'
+import * as Util from './util/Util'
 
 class App extends Component {
+  componentDidMount() {
+    API.getCategories().then((data) => {
+      API.getCategoryPosts(data[0]).then((posts) => {
+        API.getPostDetail(posts[0]).then((detail) => {
+          console.log(detail)
+        })
+        console.log(Util.uuidv4())
+      })
+    });
+
+    // API.createPost({
+    //   "id": 123473829105783,
+    //   "timestamp": Date.now(),
+    //   "title": "for fun",
+    //   "body": "for test",
+    //   "author": "wither_mo",
+    //   "category": "react"
+    // }).then((data)=>console.log(data));
+  }
+
   render() {
     return (
       <div className="App">
---
+++ frontend/src/api/Api.js
@@ -0,0 +1,92 @@
+const backEndLink = 'http://localhost:3001';
+const headers = {
+    'Authorization': 'witcher_mo',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+
+export const getCategories = () =>
+    fetch(`${backEndLink}/categories`, { headers: headers })
+        .then((res) => res.json() )
+        .then((data) => data.categories);
+
+export const getCategoryPosts = (categorie) =>
+    fetch(`${backEndLink}/${categorie.path}/posts`
+        , {headers: headers})
+        .then((res) => res.json() )
+
+export const getPosts = () =>
+    fetch(`${backEndLink}/posts`
+        , {headers: headers})
+        .then((res) => res.json() )
+
+export const createPost = (body) =>
+  fetch(`${backEndLink}/posts`, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(body)
+  }).then(res => res.json())
+
+export const getPostDetail = (post) =>
+  fetch(`${backEndLink}/posts/${post.id}`, { headers })
+    .then(res => res.json())
+
+export const votePost = (post, isUp) => {
+  const vote = isUp? "upVote" : "downVote";
+  const body = {"option": vote};
+
+  fetch(`${backEndLink}/posts/${post.id}`, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(body)
+  }).then(res => res.json())
+}
+
+export const updatePost = (post, body) =>
+  fetch(`${backEndLink}/posts/${post.id}`, {
+    method: 'PUT',
+    headers: headers,
+    body: JSON.stringify(body)
+  }).then(res => res.json())
+
+export const deletePost = (post) =>
+  fetch(`${backEndLink}/posts/${post.id}`, {
+    method: 'DELETE',
+    headers: headers,
+  }).then(res => res.json())
+
+export const createComment = (body) =>
+  fetch(`${backEndLink}/comments`, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(body)
+  }).then(res => res.json())
+
+export const voteComment = (comment, isUp) => {
+  const vote = isUp? "upVote" : "downVote";
+  const body = {"option": vote};
+
+  fetch(`${backEndLink}/comments/${comment.id}`, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(body)
+  }).then(res => res.json())
+}
+
+export const getCommentDetail = (comment) =>
+  fetch(`${backEndLink}/comments/${comment.id}`, { headers })
+    .then(res => res.json())
+
+//MARK: 用 new comment 代替 body
+export const updateComment = (comment, body) =>
+  fetch(`${backEndLink}/comments/${comment.id}`, {
+    method: 'PUT',
+    headers: headers,
+    body: JSON.stringify(body)
+  }).then(res => res.json())
+
+export const deleteComment = (comment) =>
+  fetch(`${backEndLink}/comments/${comment.id}`, {
+    method: 'DELETE',
+    headers: headers,
+  }).then(res => res.json())
